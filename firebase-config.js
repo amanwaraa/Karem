07a2547c8e2a74e3a96ea58bc4d17fd7c7f33197
Maroshop <homworkhhh25@gmail.com>
2026-04-27 15:21:59 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+window.CASHIER_FIREBASE_CONFIG = {
+  firebaseConfig: {
+    apiKey: "AIzaSyB_i6ijETJoYdvKHuu8y_sbzDMcBUnPeac",
+    authDomain: "karem-611ef.firebaseapp.com",
+    databaseURL: "https://karem-611ef-default-rtdb.firebaseio.com",
+    projectId: "karem-611ef",
+    storageBucket: "karem-611ef.firebasestorage.app",
+    messagingSenderId: "594439938004",
+    appId: "1:594439938004:web:a3fcd11b6045b08f628041",
+    measurementId: "G-V0ML8SCBE4"
+  },
+
+  firebaseRoot: "cashier_units_pro_karem_885766842",
+
+  fallbackRoots: [
+    "cashier_units_pro_karem_885766842"
+  ]
+};
